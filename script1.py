24748bd5b1614381b4d0901d847ff4ce2e32208b
7stratis <fabio.viola.italia@icloud.com>
2021-04-16 07:16:13 -0400

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,52 +1,34 @@
 import subprocess
-# import os
-
-# import subprocess
-# test = subprocess.Popen(["scutil","--set","HostName","oyo"], stdout=subprocess.PIPE)
-# import os
-# os.system("softwareupdate -l")
-
 
 data1 = subprocess.Popen(['sudo', '-nl'], stdout = subprocess.PIPE)
 output1=data1.communicate()
-# ('', None)
 if output1==('', None):
-	# print True
+	print "Please login through sudo user"
 	exit()
-else:
-	print False
-	# print output1
 
 data = subprocess.Popen(['softwareupdate', '-l'], stdout = subprocess.PIPE)
 output = data.communicate()
 print output[0]
-# print output
+if(output!=('Software Update Tool\n\nFinding available software\n', None)):
+	print("yes updates")
+	c=raw_input("If you want to install all the updates?")
+	if(c=="Y" or c=="y"):
+		data1 = subprocess.Popen(['softwareupdate', '-ia'], stdout = subprocess.PIPE)
+		output1 = data1.communicate()
+		exit()
 
-c=raw_input("If you want to install all the updates?")
-if(c=="Y" or c=="y"):
-	data1 = subprocess.Popen(['softwareupdate', '-ia'], stdout = subprocess.PIPE)
-	output1 = data1.communicate()
-	exit()
-else:
-	hostname=raw_input("enter the hostname")
-	LocalHostName=raw_input("enter the LocalHostName")
-	ComputerName=raw_input("enter the ComputerName")
+hostname=raw_input("enter the hostname")
+LocalHostName=raw_input("enter the LocalHostName")
+ComputerName=raw_input("enter the ComputerName")
 
-	test = subprocess.Popen(["scutil","--set","HostName",hostname], stdout=subprocess.PIPE)
-	outputm = test.communicate()[0]
-	print outputm
-	test = subprocess.Popen(["scutil","--set","ComputerName",ComputerName], stdout=subprocess.PIPE)
-	outputm = test.communicate()[0]
-	print outputm
-	test = subprocess.Popen(["scutil","--set","LocalHostName",LocalHostName], stdout=subprocess.PIPE)
-	outputm = test.communicate()[0]
-	print outputm
+test = subprocess.Popen(["scutil","--set","HostName",hostname], stdout=subprocess.PIPE)
+outputm = test.communicate()[0]
+print outputm
+test = subprocess.Popen(["scutil","--set","ComputerName",ComputerName], stdout=subprocess.PIPE)
+outputm = test.communicate()[0]
+print outputm
+test = subprocess.Popen(["scutil","--set","LocalHostName",LocalHostName], stdout=subprocess.PIPE)
+outputm = test.communicate()[0]
+print outputm
 
-	exit()
-
-	
-# coool="""{}""".format(output[0])
-# print coool
-# formatted_output = output.replace('\\n', '\n').replace('\\t', '\t')
-# print formatted_output
-# print output
+exit()
