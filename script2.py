6b681fa11a24aa66da4b16a762a6addd0f00bd64
7stratis <fabio.viola.italia@icloud.com>
2021-04-16 08:12:45 -0400

    Update script2.py

--- script2.py
+++ script2.py
@@ -6,19 +6,8 @@ if output1==('', None):
 	print "Please run the script with sudo"
 	exit()
 
-#data = subprocess.Popen(['softwareupdate', '-l'], stdout = subprocess.PIPE)
-#output = data.communicate()
-#print output[0]
-#if(output!=('Software Update Tool\n\nFinding available software\n', None)):
-	#print("Software Updates available")
-	#c=raw_input("Do you want to install the Software Updates? [Y/n]?: ")
-	#if(c=="Y" or c=="y"):
-		#data1 = subprocess.Popen(['softwareupdate', '-ia'], stdout = subprocess.PIPE)
-		#output1 = data1.communicate()
-		#exit()
-
 hostname=raw_input("Enter the hostname: ")
-LocalHostName=raw_input("Enter the LocalHostName :")
+LocalHostName=raw_input("Enter the LocalHostName: ")
 ComputerName=raw_input("Enter the ComputerName: ")
 
 test = subprocess.Popen(["scutil","--set","HostName",hostname], stdout=subprocess.PIPE)
