30f6b3882d4d9a6ba8e7819e5d6d14657f773d05
7stratis <fabio.viola.italia@icloud.com>
2021-05-11 12:20:48 -0400

    Update script1.py

--- script1.py
+++ script1.py
@@ -16,19 +16,3 @@ if(output!=('Software Update Tool\n\nFinding available software\n', None)):
 		data1 = subprocess.Popen(['softwareupdate', '-ia'], stdout = subprocess.PIPE)
 		output1 = data1.communicate()
 		exit()
-
-#hostname=raw_input("Enter the hostname: ")
-#LocalHostName=raw_input("Enter the LocalHostName :")
-#ComputerName=raw_input("Enter the ComputerName: ")
-
-#test = subprocess.Popen(["scutil","--set","HostName",hostname], stdout=subprocess.PIPE)
-#outputm = test.communicate()[0]
-#print outputm
-#test = subprocess.Popen(["scutil","--set","ComputerName",ComputerName], stdout=subprocess.PIPE)
-#outputm = test.communicate()[0]
-#print outputm
-#test = subprocess.Popen(["scutil","--set","LocalHostName",LocalHostName], stdout=subprocess.PIPE)
-#outputm = test.communicate()[0]
-#print outputm
-
-#exit()
